1c680c2db761092b1a2b4a3c0968f6c3499af84b
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-12-05 05:47:55 +0700

    Fix Modules Not Found

--- customize.sh
+++ customize.sh
@@ -138,7 +138,7 @@ if [[ -z "$MagiskBase" ]] && [[ -d /data/adb/ksu  ]];then
     MagiskBase="/data/adb"
 fi
 echo "$MagiskBase/modules" > $MODPATH/system/etc/XCore/info/magisk_path
-OldModolPath="$MagiskBase/modules/ZyC-Turbo-New"
+OldModolPath="$MagiskBase/modules/X-Faster"
 
 if [[ "$MODPATH" == *"modules_update"* ]] && [[ ! -z "$OldModolPath" ]];then
     ## copy system.prop
